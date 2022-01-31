9906544b91fb5fb6d6ee3047214ca966488e4313
clarkyangjw <clark@spaice.ca>
2022-01-31 16:17:19 -0500

    setup terraform

--- staging-environment/main.tf
+++ staging-environment/main.tf
@@ -17,16 +17,3 @@ terraform {
     }
     required_version = ">= 0.14.9"
 }
-provider "aws" {
-    profile = "default"
-    region  = "us-east-2"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-03a0c45ebc70f98ea"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = var.instance_name
-  }
-}
